MUSIC-fluid/MUSIC

commit a4cd762233f97f43120c60829f672572a3289d4e

Fixed all unused-but-set-variable warnings

advance.cpp
@@ -34,7 +34,8 @@ Advance::~Advance()
 
 int Advance::AdvanceIt(double tau, InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***Rneighbor, int rk_flag, int size, int rank)
 {
-  int ix, iy, ieta, flag;
+  int ix, iy, ieta;
+//   int flag;
   //cout << "0 AdvanceIt Wmunu=" << (Lneighbor[1][1][0]).Wmunu[rk_flag][1][1] << endl;
   
   for(ix=0; ix<=DATA->nx; ix++)
@@ -43,7 +44,7 @@ int Advance::AdvanceIt(double tau, InitData *DATA, Grid ***arena, Grid ***Lneigh
 	{
           for(ieta=0; ieta<DATA->neta; ieta++)
 	    {
-	      flag = AdvanceLocalT(tau, DATA, &(arena[ix][iy][ieta]), &(Lneighbor[ix][iy][0]), &(Rneighbor[ix][iy][0]), 
+	      AdvanceLocalT(tau, DATA, &(arena[ix][iy][ieta]), &(Lneighbor[ix][iy][0]), &(Rneighbor[ix][iy][0]), 
 				   &(Lneighbor[ix][iy][1]), &(Rneighbor[ix][iy][1]), rk_flag, size, rank);
 	    }/* ieta */
 	}/*iy */
@@ -59,7 +60,7 @@ int Advance::AdvanceIt(double tau, InitData *DATA, Grid ***arena, Grid ***Lneigh
 	    {
 	      for(ieta=0; ieta<DATA->neta; ieta++)
 		{
-		  flag = AdvanceLocalW(tau, DATA, &(arena[ix][iy][ieta]), &(Lneighbor[ix][iy][0]), &(Rneighbor[ix][iy][0]), 
+		  AdvanceLocalW(tau, DATA, &(arena[ix][iy][ieta]), &(Lneighbor[ix][iy][0]), &(Rneighbor[ix][iy][0]), 
 				       &(Lneighbor[ix][iy][1]), &(Rneighbor[ix][iy][1]), rk_flag, size, rank);
 		}/* ieta */
 	    }/*iy */
@@ -580,15 +581,16 @@ double *qi, double *rhs, double **w_rhs, double **qirk, Grid *grid_rk, int size,
 int Advance::FirstRKStepW(double tau, InitData *DATA, Grid *grid_pt, Grid *Lneighbor, Grid *Rneighbor, Grid *Lneighbor2, Grid *Rneighbor2, int rk_flag, 
 double *qi, double *rhs, double **w_rhs, double **qirk, Grid *grid_rk, int size, int rank)
 { 
- double tau_now, tau_next, tempf, p_rhs, temp_mu, temps, eps_ratio, tempd, tau_rk;
+ double tau_now, tau_next, tempf, p_rhs, temp_mu, temps, eps_ratio, tempd;
+//  double tau_rk;
  int alpha, flag, mu, nu, revert_flag;
  static int ind=0;
 
  tau_now = tau;
  tau_next = tau + (DATA->delta_tau);
  
- if(rk_flag == 0) {tau_rk = tau_now;}
- else if(rk_flag > 0) {tau_rk = tau_next;}
+//  if(rk_flag == 0) {tau_rk = tau_now;}
+//  else if(rk_flag > 0) {tau_rk = tau_next;}
 
 /*
  Solve partial_a (u^a W^{mu nu}) = 0
@@ -772,7 +774,7 @@ revert_flag =
 void Advance::UpdateTJbRK(Grid *grid_rk, Grid *grid_pt, InitData *DATA, int rk_flag)
 {
  int trk_flag, mu, alpha;
- double tempf;
+//  double tempf;
 
  trk_flag = rk_flag+1;
 
@@ -783,7 +785,7 @@ void Advance::UpdateTJbRK(Grid *grid_rk, Grid *grid_pt, InitData *DATA, int rk_f
   /* reconstructed grid_rk uses rk_flag 0 only */
    for(mu=0; mu<4; mu++)
     {
-     tempf = grid_pt->u[trk_flag][mu] = grid_rk->u[0][mu];
+//      tempf = grid_pt->u[trk_flag][mu] = grid_rk->u[0][mu];
      for(alpha=0; alpha<5; alpha++)
       {
         grid_pt->TJb[trk_flag][alpha][mu] = grid_rk->TJb[0][alpha][mu];
@@ -805,7 +807,7 @@ int Advance::QuestRevert(double tau, int add, Grid *grid_pt, int rk_flag, InitDa
  //factor = 10.*pow(tanh(tau)/tau,2.5)+1.; //20, 0.5
  factor=2.;
  // cout << "factor=" << factor << endl;
- int whichCorr=0;
+//  int whichCorr=0;
  
  revert_flag = 0;
 
@@ -822,36 +824,36 @@ int Advance::QuestRevert(double tau, int add, Grid *grid_pt, int rk_flag, InitDa
  
  rho=max(fabs(trace)/fabs(grid_pt->epsilon+3.*grid_pt->p)/factor/0.5,rho); //factor/0.01
 
- whichCorr=1;
+//  whichCorr=1;
  
  // correct transversality
  trans = -grid_pt->u[rk_flag+1][0]*grid_pt->Wmunu[rk_flag+1][0][1]
    +grid_pt->u[rk_flag+1][1]*grid_pt->Wmunu[rk_flag+1][1][1]
    +grid_pt->u[rk_flag+1][2]*grid_pt->Wmunu[rk_flag+1][2][1]
    +grid_pt->u[rk_flag+1][3]*grid_pt->Wmunu[rk_flag+1][3][1];
- if(fabs(trans)/ezero/factor>rho)
-   whichCorr=2;
+//  if(fabs(trans)/ezero/factor>rho)
+//    whichCorr=2;
  rho=max(fabs(trans)/ezero/factor,rho);
  trans = -grid_pt->u[rk_flag+1][0]*grid_pt->Wmunu[rk_flag+1][0][2]
    +grid_pt->u[rk_flag+1][1]*grid_pt->Wmunu[rk_flag+1][1][2]
    +grid_pt->u[rk_flag+1][2]*grid_pt->Wmunu[rk_flag+1][2][2]
    +grid_pt->u[rk_flag+1][3]*grid_pt->Wmunu[rk_flag+1][3][2];
- if(fabs(trans)/ezero/factor>rho)
-   whichCorr=3;
+//  if(fabs(trans)/ezero/factor>rho)
+//    whichCorr=3;
  rho=max(fabs(trans)/ezero/factor,rho);
  trans = -grid_pt->u[rk_flag+1][0]*grid_pt->Wmunu[rk_flag+1][0][3]
    +grid_pt->u[rk_flag+1][1]*grid_pt->Wmunu[rk_flag+1][1][3]
    +grid_pt->u[rk_flag+1][2]*grid_pt->Wmunu[rk_flag+1][2][3]
    +grid_pt->u[rk_flag+1][3]*grid_pt->Wmunu[rk_flag+1][3][3]/tau;
- if(fabs(trans)/ezero/factor>rho)
-   whichCorr=4;
+//  if(fabs(trans)/ezero/factor>rho)
+//    whichCorr=4;
  rho=max(fabs(trans)/ezero/factor,rho);
  trans = -grid_pt->u[rk_flag+1][0]*grid_pt->Wmunu[rk_flag+1][0][0]
    +grid_pt->u[rk_flag+1][1]*grid_pt->Wmunu[rk_flag+1][1][0]
    +grid_pt->u[rk_flag+1][2]*grid_pt->Wmunu[rk_flag+1][2][0]
    +grid_pt->u[rk_flag+1][3]*grid_pt->Wmunu[rk_flag+1][3][0];
- if(fabs(trans)/ezero/factor>rho)
-   whichCorr=5;
+//  if(fabs(trans)/ezero/factor>rho)
+//    whichCorr=5;
  rho=max(fabs(trans)/ezero/factor,rho);
 
  // correct size
@@ -873,8 +875,8 @@ int Advance::QuestRevert(double tau, int add, Grid *grid_pt, int rk_flag, InitDa
        +grid_pt->Wmunu[rk_flag+1][2][3]*grid_pt->Wmunu[rk_flag+1][2][3]
        ));
 
- if(sqrt(pisize)/(sqrt((grid_pt->epsilon*grid_pt->epsilon+3.*grid_pt->p*grid_pt->p)))/factor>rho)
-   whichCorr=6;
+//  if(sqrt(pisize)/(sqrt((grid_pt->epsilon*grid_pt->epsilon+3.*grid_pt->p*grid_pt->p)))/factor>rho)
+//    whichCorr=6;
 
  // rho =  max(fabs(grid_pt->Wmunu[rk_flag+1][1][1] + grid_pt->Pimunu[rk_flag+1][1][1])/grid_pt->TJb[rk_flag+1][1][1]/factor,rho);
  //rho =  max(fabs(grid_pt->Wmunu[rk_flag+1][2][2] + grid_pt->Pimunu[rk_flag+1][2][2])/grid_pt->TJb[rk_flag+1][2][2]/factor,rho);
@@ -932,13 +934,13 @@ int Advance::QuestRevert(double tau, int add, Grid *grid_pt, int rk_flag, InitDa
     //  cout << "reverting. epsilon=" <<  grid_pt->epsilon << endl;
      double x=grid_pt->position[1]*DATA->delta_x-DATA->x_size/2;
      double y=grid_pt->position[2]*DATA->delta_y-DATA->y_size/2;
-     double eta;
-     if(size>1)
-       eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2+static_cast<double>(rank)/static_cast<double>(size)*DATA->eta_size;
-     else
-       eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2;
-     double r;
-     r=sqrt(x*x+y*y);
+//      double eta;
+//      if(size>1)
+//        eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2+static_cast<double>(rank)/static_cast<double>(size)*DATA->eta_size;
+//      else
+//        eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2;
+//      double r;
+//      r=sqrt(x*x+y*y);
 //      if( grid_pt->epsilon > 10. ) // was 0.3 , changed by Schenke 2010/07/16  
 //        {
 // 	 fprintf(stderr, 
@@ -1297,19 +1299,20 @@ void Advance::MakeDeltaQI(double tau, Grid *grid_pt, Grid *Lneighbor, Grid *Rnei
 {
   double xjet, yjet, xtrigger, ytrigger;
   static double delta[4], sumf;
-  static double tau_fac[4];
-  static int alpha, i, rk_order_m1, nmax[4], flag;
+//   static double tau_fac[4];
+  static int alpha, i, rk_order_m1;
+//   static int nmax[4], flag;
   static double **DFmmp;
   static NbrQs NbrCells;
   static BdryCells HalfwayCells;
   static int ind=0;
   double x=grid_pt->position[1]*DATA->delta_x-DATA->x_size/2;
   double y=grid_pt->position[2]*DATA->delta_y-DATA->y_size/2;
-  double eta;
-  if(size>1)
-    eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2+static_cast<double>(rank)/static_cast<double>(size)*DATA->eta_size;
-  else
-    eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2;
+//   double eta;
+//   if(size>1)
+//     eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2+static_cast<double>(rank)/static_cast<double>(size)*DATA->eta_size;
+//   else
+//     eta=grid_pt->position[3]*DATA->delta_eta-DATA->eta_size/2;
   
   ind++;
   if(ind==1)
@@ -1323,9 +1326,9 @@ void Advance::MakeDeltaQI(double tau, Grid *grid_pt, Grid *Lneighbor, Grid *Rnei
   delta[2] = DATA->delta_y;
   delta[3] = DATA->delta_eta;
   
-  nmax[1] = DATA->nx;
-  nmax[2] = DATA->ny;
-  nmax[3] = DATA->neta-1;
+//   nmax[1] = DATA->nx;
+//   nmax[2] = DATA->ny;
+//   nmax[3] = DATA->neta-1;
 
 /* \partial_tau (tau Ttautau) + \partial_eta Tetatau 
            + \partial_x (tau Txtau) + \partial_y (tau Tytau) + Tetaeta = 0 */
@@ -1334,9 +1337,9 @@ void Advance::MakeDeltaQI(double tau, Grid *grid_pt, Grid *Lneighbor, Grid *Rnei
 /* \partial_tau (tau Txtau) + \partial_eta Tetax + \partial_x tau T_xx
   + \partial_y tau Tyx = 0 */
 
-     tau_fac[1] = tau;
-     tau_fac[2] = tau;
-     tau_fac[3] = 1.0;
+//      tau_fac[1] = tau;
+//      tau_fac[2] = tau;
+//      tau_fac[3] = 1.0;
  
  for(alpha=0; alpha<5; alpha++) 
   {
@@ -1346,10 +1349,10 @@ void Advance::MakeDeltaQI(double tau, Grid *grid_pt, Grid *Lneighbor, Grid *Rnei
  /* implement Kurganov-Tadmor scheme */
  GetQIs(tau, grid_pt, Lneighbor, Rneighbor, Lneighbor2, Rneighbor2, qi, &NbrCells, rk_flag, DATA, size, rank);
  
- flag = 
+//  flag = 
    MakeQIHalfs(qi, &NbrCells, &HalfwayCells, grid_pt, DATA);
  
- flag = 
+//  flag = 
    ConstHalfwayCells(tau, &HalfwayCells, qi, grid_pt, DATA, rk_flag, size, rank);
  
  MakeKTCurrents(tau, DFmmp, grid_pt, &HalfwayCells, rk_flag);
@@ -1642,14 +1645,15 @@ void Advance::GetQIs
 int Advance::MakeQIHalfs(double *qi, NbrQs *NbrCells, BdryCells *HalfwayCells, 
 			 Grid *grid_pt, InitData *DATA)
 {
- int alpha, direc, nmax[4], flag;
+ int alpha, direc, flag;
+//  int nmax[4];
  double fphL, fphR, fmhL, fmhR;
  double gphL, gphR, gmhL, gmhR;
  double x, y, eta, tempf;
 
-  nmax[1] = DATA->nx;
-  nmax[2] = DATA->ny;
-  nmax[3] = DATA->neta-1;
+//   nmax[1] = DATA->nx;
+//   nmax[2] = DATA->ny;
+//   nmax[3] = DATA->neta-1;
   
   for(alpha=0; alpha<5; alpha++)
   {

eos.cpp
@@ -35,7 +35,7 @@ void EOS::init_eos()
   // baryon chemical potential from file
   whichEOS = 1;
   fprintf(stderr,"reading EOS... \n");
-  int bytes_read;
+//   int bytes_read;
   int i, j;
   FILE *eos_p1, *eos_p2;
   FILE *eos_T1, *eos_T2;
@@ -122,18 +122,18 @@ void EOS::init_eos()
   //read the first two lines:
   // first value of rhob, first value of epsilon
   // deltaRhob, deltaEpsilon, number of rhob steps, number of epsilon steps
-  bytes_read=fscanf(eos_p1,"%lf %lf",&BNP1,&EPP1);
-  bytes_read=fscanf(eos_p1,"%lf %lf %d %d",&deltaBNP1,&deltaEPP1,&NBNP1,&NEPP1);
-  bytes_read=fscanf(eos_p2,"%lf %lf",&BNP2,&EPP2);
-  bytes_read=fscanf(eos_p2,"%lf %lf %d %d",&deltaBNP2,&deltaEPP2,&NBNP2,&NEPP2);
-  bytes_read=fscanf(eos_T1,"%lf %lf",&BNP1,&EPP1);
-  bytes_read=fscanf(eos_T1,"%lf %lf %d %d",&deltaBNP1,&deltaEPP1,&NBNP1,&NEPP1);
-  bytes_read=fscanf(eos_T2,"%lf %lf",&BNP2,&EPP2);
-  bytes_read=fscanf(eos_T2,"%lf %lf %d %d",&deltaBNP2,&deltaEPP2,&NBNP2,&NEPP2);
-  bytes_read=fscanf(eos_mu1,"%lf %lf",&BNP1,&EPP1);
-  bytes_read=fscanf(eos_mu1,"%lf %lf %d %d",&deltaBNP1,&deltaEPP1,&NBNP1,&NEPP1);
-  bytes_read=fscanf(eos_mu2,"%lf %lf",&BNP2,&EPP2);
-  bytes_read=fscanf(eos_mu2,"%lf %lf %d %d",&deltaBNP2,&deltaEPP2,&NBNP2,&NEPP2);
+  fscanf(eos_p1,"%lf %lf",&BNP1,&EPP1);
+  fscanf(eos_p1,"%lf %lf %d %d",&deltaBNP1,&deltaEPP1,&NBNP1,&NEPP1);
+  fscanf(eos_p2,"%lf %lf",&BNP2,&EPP2);
+  fscanf(eos_p2,"%lf %lf %d %d",&deltaBNP2,&deltaEPP2,&NBNP2,&NEPP2);
+  fscanf(eos_T1,"%lf %lf",&BNP1,&EPP1);
+  fscanf(eos_T1,"%lf %lf %d %d",&deltaBNP1,&deltaEPP1,&NBNP1,&NEPP1);
+  fscanf(eos_T2,"%lf %lf",&BNP2,&EPP2);
+  fscanf(eos_T2,"%lf %lf %d %d",&deltaBNP2,&deltaEPP2,&NBNP2,&NEPP2);
+  fscanf(eos_mu1,"%lf %lf",&BNP1,&EPP1);
+  fscanf(eos_mu1,"%lf %lf %d %d",&deltaBNP1,&deltaEPP1,&NBNP1,&NEPP1);
+  fscanf(eos_mu2,"%lf %lf",&BNP2,&EPP2);
+  fscanf(eos_mu2,"%lf %lf %d %d",&deltaBNP2,&deltaEPP2,&NBNP2,&NEPP2);
  
   // allocate memory for pressure arrays
   pressure1=util->mtx_malloc(NBNP1+1,NEPP1+1);
@@ -152,17 +152,17 @@ void EOS::init_eos()
   for(j=0;j<=NEPP1;j++)
     for(i=0;i<=NBNP1;i++)
       {
-	bytes_read=fscanf(eos_p1,"%lf",&pressure1[i][j]);
-	bytes_read=fscanf(eos_T1,"%lf",&temperature1[i][j]);
-	bytes_read=fscanf(eos_mu1,"%lf",&mu1[i][j]);
+	fscanf(eos_p1,"%lf",&pressure1[i][j]);
+	fscanf(eos_T1,"%lf",&temperature1[i][j]);
+	fscanf(eos_mu1,"%lf",&mu1[i][j]);
       }
 
   for(j=0;j<=NEPP2;j++)
     for(i=0;i<=NBNP2;i++)
       {
-	bytes_read=fscanf(eos_p2,"%lf",&pressure2[i][j]);
-	bytes_read=fscanf(eos_T2,"%lf",&temperature2[i][j]);
-	bytes_read=fscanf(eos_mu2,"%lf",&mu2[i][j]);
+	fscanf(eos_p2,"%lf",&pressure2[i][j]);
+	fscanf(eos_T2,"%lf",&temperature2[i][j]);
+	fscanf(eos_mu2,"%lf",&mu2[i][j]);
       }
 
   fclose(eos_p1);
@@ -376,34 +376,34 @@ void EOS::init_eos2()
   //read the first two lines with general info:
   // lowest value of epsilon
   // deltaEpsilon, number of epsilon steps (i.e. # of lines)
-  bytes_read=fscanf(eos_T1,"%lf",&EPP1);
-  bytes_read=fscanf(eos_T1,"%lf %d",&deltaEPP1,&NEPP1);
-  bytes_read=fscanf(eos_T2,"%lf",&EPP2);
-  bytes_read=fscanf(eos_T2,"%lf %d",&deltaEPP2,&NEPP2);
-  bytes_read=fscanf(eos_T3,"%lf",&EPP3);
-  bytes_read=fscanf(eos_T3,"%lf %d",&deltaEPP3,&NEPP3);
-  bytes_read=fscanf(eos_T4,"%lf",&EPP4);
-  bytes_read=fscanf(eos_T4,"%lf %d",&deltaEPP4,&NEPP4);
-  bytes_read=fscanf(eos_T5,"%lf",&EPP5);
-  bytes_read=fscanf(eos_T5,"%lf %d",&deltaEPP5,&NEPP5);
-  bytes_read=fscanf(eos_T6,"%lf",&EPP6);
-  bytes_read=fscanf(eos_T6,"%lf %d",&deltaEPP6,&NEPP6);
-  bytes_read=fscanf(eos_T7,"%lf",&EPP7);
-  bytes_read=fscanf(eos_T7,"%lf %d",&deltaEPP7,&NEPP7);
-  bytes_read=fscanf(eos_d1,"%lf",&EPP1);
-  bytes_read=fscanf(eos_d1,"%lf %d",&deltaEPP1,&NEPP1);
-  bytes_read=fscanf(eos_d2,"%lf",&EPP2);
-  bytes_read=fscanf(eos_d2,"%lf %d",&deltaEPP2,&NEPP2);
-  bytes_read=fscanf(eos_d3,"%lf",&EPP3);
-  bytes_read=fscanf(eos_d3,"%lf %d",&deltaEPP3,&NEPP3);
-  bytes_read=fscanf(eos_d4,"%lf",&EPP4);
-  bytes_read=fscanf(eos_d4,"%lf %d",&deltaEPP4,&NEPP4);
-  bytes_read=fscanf(eos_d5,"%lf",&EPP5);
-  bytes_read=fscanf(eos_d5,"%lf %d",&deltaEPP5,&NEPP5);
-  bytes_read=fscanf(eos_d6,"%lf",&EPP6);
-  bytes_read=fscanf(eos_d6,"%lf %d",&deltaEPP6,&NEPP6);
-  bytes_read=fscanf(eos_d7,"%lf",&EPP7);
-  bytes_read=fscanf(eos_d7,"%lf %d",&deltaEPP7,&NEPP7);
+  fscanf(eos_T1,"%lf",&EPP1);
+  fscanf(eos_T1,"%lf %d",&deltaEPP1,&NEPP1);
+  fscanf(eos_T2,"%lf",&EPP2);
+  fscanf(eos_T2,"%lf %d",&deltaEPP2,&NEPP2);
+  fscanf(eos_T3,"%lf",&EPP3);
+  fscanf(eos_T3,"%lf %d",&deltaEPP3,&NEPP3);
+  fscanf(eos_T4,"%lf",&EPP4);
+  fscanf(eos_T4,"%lf %d",&deltaEPP4,&NEPP4);
+  fscanf(eos_T5,"%lf",&EPP5);
+  fscanf(eos_T5,"%lf %d",&deltaEPP5,&NEPP5);
+  fscanf(eos_T6,"%lf",&EPP6);
+  fscanf(eos_T6,"%lf %d",&deltaEPP6,&NEPP6);
+  fscanf(eos_T7,"%lf",&EPP7);
+  fscanf(eos_T7,"%lf %d",&deltaEPP7,&NEPP7);
+  fscanf(eos_d1,"%lf",&EPP1);
+  fscanf(eos_d1,"%lf %d",&deltaEPP1,&NEPP1);
+  fscanf(eos_d2,"%lf",&EPP2);
+  fscanf(eos_d2,"%lf %d",&deltaEPP2,&NEPP2);
+  fscanf(eos_d3,"%lf",&EPP3);
+  fscanf(eos_d3,"%lf %d",&deltaEPP3,&NEPP3);
+  fscanf(eos_d4,"%lf",&EPP4);
+  fscanf(eos_d4,"%lf %d",&deltaEPP4,&NEPP4);
+  fscanf(eos_d5,"%lf",&EPP5);
+  fscanf(eos_d5,"%lf %d",&deltaEPP5,&NEPP5);
+  fscanf(eos_d6,"%lf",&EPP6);
+  fscanf(eos_d6,"%lf %d",&deltaEPP6,&NEPP6);
+  fscanf(eos_d7,"%lf",&EPP7);
+  fscanf(eos_d7,"%lf %d",&deltaEPP7,&NEPP7);
   
  
   // no rho_b dependence at the moment
@@ -463,86 +463,86 @@ void EOS::init_eos2()
   i=0;
   for(j=NEPP1-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d1,"%lf",&eps);
-      bytes_read=fscanf(eos_d1,"%lf",&pressure1[i][j]);
-      bytes_read=fscanf(eos_d1,"%lf",&entropyDensity1[i][j]);
-      bytes_read=fscanf(eos_d1,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d1,"%lf",&QGPfraction1[i][j]);
-      bytes_read=fscanf(eos_T1,"%lf",&temperature1[i][j]);
-      bytes_read=fscanf(eos_T1,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T1,"%lf",&eps); //dummy
+      fscanf(eos_d1,"%lf",&eps);
+      fscanf(eos_d1,"%lf",&pressure1[i][j]);
+      fscanf(eos_d1,"%lf",&entropyDensity1[i][j]);
+      fscanf(eos_d1,"%lf",&baryonDensity);
+      fscanf(eos_d1,"%lf",&QGPfraction1[i][j]);
+      fscanf(eos_T1,"%lf",&temperature1[i][j]);
+      fscanf(eos_T1,"%lf",&eps); //dummy
+      fscanf(eos_T1,"%lf",&eps); //dummy
     }
 
   for(j=NEPP2-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d2,"%lf",&eps);
-      bytes_read=fscanf(eos_d2,"%lf",&pressure2[i][j]);
-      bytes_read=fscanf(eos_d2,"%lf",&entropyDensity2[i][j]);
-      bytes_read=fscanf(eos_d2,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d2,"%lf",&QGPfraction2[i][j]);
-      bytes_read=fscanf(eos_T2,"%lf",&temperature2[i][j]);
-      bytes_read=fscanf(eos_T2,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T2,"%lf",&eps); //dummy
+      fscanf(eos_d2,"%lf",&eps);
+      fscanf(eos_d2,"%lf",&pressure2[i][j]);
+      fscanf(eos_d2,"%lf",&entropyDensity2[i][j]);
+      fscanf(eos_d2,"%lf",&baryonDensity);
+      fscanf(eos_d2,"%lf",&QGPfraction2[i][j]);
+      fscanf(eos_T2,"%lf",&temperature2[i][j]);
+      fscanf(eos_T2,"%lf",&eps); //dummy
+      fscanf(eos_T2,"%lf",&eps); //dummy
     }
 
   for(j=NEPP3-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d3,"%lf",&eps);
-      bytes_read=fscanf(eos_d3,"%lf",&pressure3[i][j]);
-      bytes_read=fscanf(eos_d3,"%lf",&entropyDensity3[i][j]);
-      bytes_read=fscanf(eos_d3,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d3,"%lf",&QGPfraction3[i][j]);
-      bytes_read=fscanf(eos_T3,"%lf",&temperature3[i][j]);
-      bytes_read=fscanf(eos_T3,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T3,"%lf",&eps); //dummy
+      fscanf(eos_d3,"%lf",&eps);
+      fscanf(eos_d3,"%lf",&pressure3[i][j]);
+      fscanf(eos_d3,"%lf",&entropyDensity3[i][j]);
+      fscanf(eos_d3,"%lf",&baryonDensity);
+      fscanf(eos_d3,"%lf",&QGPfraction3[i][j]);
+      fscanf(eos_T3,"%lf",&temperature3[i][j]);
+      fscanf(eos_T3,"%lf",&eps); //dummy
+      fscanf(eos_T3,"%lf",&eps); //dummy
     }
 
   for(j=NEPP4-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d4,"%lf",&eps);
-      bytes_read=fscanf(eos_d4,"%lf",&pressure4[i][j]);
-      bytes_read=fscanf(eos_d4,"%lf",&entropyDensity4[i][j]);
-      bytes_read=fscanf(eos_d4,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d4,"%lf",&QGPfraction4[i][j]);
-      bytes_read=fscanf(eos_T4,"%lf",&temperature4[i][j]);
-      bytes_read=fscanf(eos_T4,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T4,"%lf",&eps); //dummy
+      fscanf(eos_d4,"%lf",&eps);
+      fscanf(eos_d4,"%lf",&pressure4[i][j]);
+      fscanf(eos_d4,"%lf",&entropyDensity4[i][j]);
+      fscanf(eos_d4,"%lf",&baryonDensity);
+      fscanf(eos_d4,"%lf",&QGPfraction4[i][j]);
+      fscanf(eos_T4,"%lf",&temperature4[i][j]);
+      fscanf(eos_T4,"%lf",&eps); //dummy
+      fscanf(eos_T4,"%lf",&eps); //dummy
     }
 
   for(j=NEPP5-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d5,"%lf",&eps);
-      bytes_read=fscanf(eos_d5,"%lf",&pressure5[i][j]);
-      bytes_read=fscanf(eos_d5,"%lf",&entropyDensity5[i][j]);
-      bytes_read=fscanf(eos_d5,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d5,"%lf",&QGPfraction5[i][j]);
-      bytes_read=fscanf(eos_T5,"%lf",&temperature5[i][j]);
-      bytes_read=fscanf(eos_T5,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T5,"%lf",&eps); //dummy
+      fscanf(eos_d5,"%lf",&eps);
+      fscanf(eos_d5,"%lf",&pressure5[i][j]);
+      fscanf(eos_d5,"%lf",&entropyDensity5[i][j]);
+      fscanf(eos_d5,"%lf",&baryonDensity);
+      fscanf(eos_d5,"%lf",&QGPfraction5[i][j]);
+      fscanf(eos_T5,"%lf",&temperature5[i][j]);
+      fscanf(eos_T5,"%lf",&eps); //dummy
+      fscanf(eos_T5,"%lf",&eps); //dummy
     } 
 
   for(j=NEPP6-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d6,"%lf",&eps);
-      bytes_read=fscanf(eos_d6,"%lf",&pressure6[i][j]);
-      bytes_read=fscanf(eos_d6,"%lf",&entropyDensity6[i][j]);
-      bytes_read=fscanf(eos_d6,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d6,"%lf",&QGPfraction6[i][j]);
-      bytes_read=fscanf(eos_T6,"%lf",&temperature6[i][j]);
-      bytes_read=fscanf(eos_T6,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T6,"%lf",&eps); //dummy
+      fscanf(eos_d6,"%lf",&eps);
+      fscanf(eos_d6,"%lf",&pressure6[i][j]);
+      fscanf(eos_d6,"%lf",&entropyDensity6[i][j]);
+      fscanf(eos_d6,"%lf",&baryonDensity);
+      fscanf(eos_d6,"%lf",&QGPfraction6[i][j]);
+      fscanf(eos_T6,"%lf",&temperature6[i][j]);
+      fscanf(eos_T6,"%lf",&eps); //dummy
+      fscanf(eos_T6,"%lf",&eps); //dummy
     } 
 
   for(j=NEPP7-1; j>=0; j--)
     {
-      bytes_read=fscanf(eos_d7,"%lf",&eps);
-      bytes_read=fscanf(eos_d7,"%lf",&pressure7[i][j]);
-      bytes_read=fscanf(eos_d7,"%lf",&entropyDensity7[i][j]);
-      bytes_read=fscanf(eos_d7,"%lf",&baryonDensity);
-      bytes_read=fscanf(eos_d7,"%lf",&QGPfraction7[i][j]);
-      bytes_read=fscanf(eos_T7,"%lf",&temperature7[i][j]);
-      bytes_read=fscanf(eos_T7,"%lf",&eps); //dummy
-      bytes_read=fscanf(eos_T7,"%lf",&eps); //dummy
+      fscanf(eos_d7,"%lf",&eps);
+      fscanf(eos_d7,"%lf",&pressure7[i][j]);
+      fscanf(eos_d7,"%lf",&entropyDensity7[i][j]);
+      fscanf(eos_d7,"%lf",&baryonDensity);
+      fscanf(eos_d7,"%lf",&QGPfraction7[i][j]);
+      fscanf(eos_T7,"%lf",&temperature7[i][j]);
+      fscanf(eos_T7,"%lf",&eps); //dummy
+      fscanf(eos_T7,"%lf",&eps); //dummy
     } 
 
 //test if the reading worked:
@@ -793,8 +793,8 @@ void EOS::init_eos3(int selector)
   eos_d7 >> deltaEPP7;
   eos_d7 >> NEPP7;
   
-  // bytes_read=fscanf(eos_d1,"%lf",&EPP1);
-  // bytes_read=fscanf(eos_d1,"%lf %d",&deltaEPP1,&NEPP1);
+  // fscanf(eos_d1,"%lf",&EPP1);
+  // fscanf(eos_d1,"%lf %d",&deltaEPP1,&NEPP1);
   // cout << "check 4" << endl;
 
   // no rho_b dependence at the moment
@@ -861,17 +861,17 @@ void EOS::init_eos3(int selector)
       eos_d1 >> entropyDensity1[i][j];
       eos_d1 >> baryonDensity;
       eos_d1 >> QGPfraction1[i][j];
-      //      bytes_read=fscanf(eos_d1,"%lf",&eps);
-      //bytes_read=fscanf(eos_d1,"%lf",&pressure1[i][j]);
-      //bytes_read=fscanf(eos_d1,"%lf",&entropyDensity1[i][j]);
-      //bytes_read=fscanf(eos_d1,"%lf",&baryonDensity);
-      //bytes_read=fscanf(eos_d1,"%lf",&QGPfraction1[i][j]);
+      //      fscanf(eos_d1,"%lf",&eps);
+      //fscanf(eos_d1,"%lf",&pressure1[i][j]);
+      //fscanf(eos_d1,"%lf",&entropyDensity1[i][j]);
+      //fscanf(eos_d1,"%lf",&baryonDensity);
+      //fscanf(eos_d1,"%lf",&QGPfraction1[i][j]);
       eos_T1 >> temperature1[i][j];
       eos_T1 >> eps;
       eos_T1 >> eps;
-      //bytes_read=fscanf(eos_T1,"%lf",&temperature1[i][j]);
-      //bytes_read=fscanf(eos_T1,"%lf",&eps); //dummy
-      //bytes_read=fscanf(eos_T1,"%lf",&eps); //dummy
+      //fscanf(eos_T1,"%lf",&temperature1[i][j]);
+      //fscanf(eos_T1,"%lf",&eps); //dummy
+      //fscanf(eos_T1,"%lf",&eps); //dummy
     }
 
   for(j=NEPP2-1; j>=0; j--)

evolve.cpp
@@ -33,7 +33,8 @@ Evolve::~Evolve()
 int Evolve::EvolveIt(InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***Rneighbor, int size, int rank)
 {
 /* implement Kurganov-Tadmor */
- int ix, iy, ieta, nx, ny, neta, it, itmax, rk_flag, flag, cent_eta;
+ int ix, iy, ieta, nx, ny, neta, it, itmax, rk_flag,  cent_eta;
+//  int flag;
  double dt, tau0, tau, x;
 
  if (DATA->output_hydro_debug_info) {
@@ -123,7 +124,7 @@ int Evolve::EvolveIt(InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***R
 
    /* execute rk steps */
   
-   flag = AdvanceRK(tau, DATA, arena, Lneighbor, Rneighbor, size, rank);
+   AdvanceRK(tau, DATA, arena, Lneighbor, Rneighbor, size, rank);
 
    UpdateArena(tau, DATA, arena);
    
@@ -167,11 +168,12 @@ int Evolve::EvolveIt(InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***R
 void Evolve::storePreviousEpsilon2(double tau, InitData *DATA, Grid ***arena)
 {
   int ix, iy, ieta, nx, ny, neta;
-  double x, y, eta, tau0;
+  double x, y, eta;
+//   double tau0;
   nx = DATA->nx;
   ny = DATA->ny;
   neta = DATA->neta;
-  tau0 = DATA->tau0;
+//   tau0 = DATA->tau0;
 
   for(ix=0; ix<=nx; ix++)
     {
@@ -195,11 +197,12 @@ void Evolve::storePreviousEpsilon2(double tau, InitData *DATA, Grid ***arena)
 void Evolve::storePreviousW(double tau, InitData *DATA, Grid ***arena)
 {
   int ix, iy, ieta, nx, ny, neta;
-  double x, y, eta, tau0;
+  double x, y, eta;
+//   double tau0;
   nx = DATA->nx;
   ny = DATA->ny;
   neta = DATA->neta;
-  tau0 = DATA->tau0;
+//   tau0 = DATA->tau0;
 
   for(ix=0; ix<=nx; ix++)
     {
@@ -225,11 +228,12 @@ void Evolve::storePreviousW(double tau, InitData *DATA, Grid ***arena)
 void Evolve::storePreviousT(double tau, InitData *DATA, Grid ***arena)
 {
   int ix, iy, ieta, nx, ny, neta;
-  double x, y, eta, tau0;
+  double x, y, eta;
+//   double tau0;
   nx = DATA->nx;
   ny = DATA->ny;
   neta = DATA->neta;
-  tau0 = DATA->tau0;
+//   tau0 = DATA->tau0;
 
   for(ix=0; ix<=nx; ix++)
     {
@@ -361,8 +365,8 @@ void Evolve::FindFreezeOutSurface(double tau, InitData *DATA, Grid ***arena, int
   FILE *t_file;
   const char* t_name = "tauf.dat";
   t_file = fopen(t_name, "a");
-  char *buf;
-  buf = util->char_malloc(40);
+//   char *buf;
+//   buf = util->char_malloc(40);
 //   FILE *s_file;
 //   char* s_name;
 //   s_name = util->char_malloc(100);
@@ -810,10 +814,11 @@ void Evolve::FindFreezeOutSurface2(double tau, InitData *DATA, Grid ***arena, in
   int previousEdges[32], usedEdges1[32],usedEdges2[32],usedEdges3[32],usedEdges4[32], countAdditionalEdges;
   int fac,l, COUNTER, additionalEdges, temp, m, m2;
   int group[32][3];
-  int tries, tries2, tries3;
+  int tries, tries3;
+//   int tries2;
   int shift;
   int addCOUNTER;
-  int ISID[32];
+//   int ISID[32];
   int countSingleEdges;
   int singleConnections[32][2];
   int singleConnectionsUsed[32];
@@ -2212,7 +2217,7 @@ void Evolve::FindFreezeOutSurface2(double tau, InitData *DATA, Grid ***arena, in
 			      if ( IBIT[IE-1][JE-1]==1 ) 
 				{
 				  IS = IS+1;
-				  ISID[IS-1] = j;
+// 				  ISID[IS-1] = j;
 				  neighbors[i][IS-1]=JE; // determines the neighbors of ith edge (which is iEdge[i])
 				}
 			    }
@@ -2223,7 +2228,7 @@ void Evolve::FindFreezeOutSurface2(double tau, InitData *DATA, Grid ***arena, in
 			      if ( IS<6 && IBIT[IE-1][JE-1]==2 ) 
 				{
 				  IS = IS+1;
-				  ISID[IS-1] = j;
+// 				  ISID[IS-1] = j;
 				  neighbors[i][IS-1]=JE; // determines the neighbors of ith edge (which is iEdge[i])
 				}
 			    }
@@ -2400,7 +2405,7 @@ void Evolve::FindFreezeOutSurface2(double tau, InitData *DATA, Grid ***arena, in
 		      countSingleEdges=1; // just to get started in the while loop at all
 		      shift=0;
 		      //addCOUNTER=0;
-		      tries2=0;
+// 		      tries2=0;
 		   /*    for(j=1; j<=32; j++) */
 /* 			for (k=1; k<j; k++) */
 /* 			  { */
@@ -4503,7 +4508,7 @@ int Evolve::FindFreezeOutSurface3(double tau, InitData *DATA, Grid ***arena, int
   int nix, niy, nieta;
   double Wtautau, Wtaux, Wtauy, Wtaueta, Wxx, Wxy, Wxeta, Wyy, Wyeta, Wetaeta;
   double rhob, utau, ux, uy, ueta, TFO, muB, eps_plus_p_over_T_FO;
-  int shown;
+//   int shown;
   
   fac=1; // Non-unity value does not currently work
   facTau = DATA->facTau;
@@ -4522,7 +4527,7 @@ int Evolve::FindFreezeOutSurface3(double tau, InitData *DATA, Grid ***arena, int
 //   fprintf(stderr,"DX=%f\n", DX);
 //   fprintf(stderr,"DY=%f\n", DY);
 //   fprintf(stderr,"DETA=%f\n", DETA);
-  shown = 0;
+//   shown = 0;
   
   int maxEta;
   
@@ -5343,9 +5348,8 @@ int Evolve::FindFreezeOutSurface3(double tau, InitData *DATA, Grid ***arena, int
 // 	    }
 // 	  
 	  
-	  int ret;
-	  ret = system("cat surface?.dat surface??.dat > surface.dat");
-	  ret = system("rm surface?.dat surface??.dat 2> /dev/null");
+	  system("cat surface?.dat surface??.dat > surface.dat");
+	  system("rm surface?.dat surface??.dat 2> /dev/null");
 	  
 // 	  MPI::Finalize();
 //  	  exit(1);

gabriel-sr151/MUSIC-runs

commit 0806eac2c2eaaa66d9f2d3089d0461eb341b25f8

Fixed bug introduced in revision a4cd762233f97f43120c60829f672572a3289d4e

advance.cpp
@@ -793,7 +793,7 @@ void Advance::UpdateTJbRK(Grid *grid_rk, Grid *grid_pt, InitData *DATA, int rk_f
   /* reconstructed grid_rk uses rk_flag 0 only */
    for(mu=0; mu<4; mu++)
     {
-//      tempf = grid_pt->u[trk_flag][mu] = grid_rk->u[0][mu];
+      grid_pt->u[trk_flag][mu] = grid_rk->u[0][mu];
      for(alpha=0; alpha<5; alpha++)
       {
         grid_pt->TJb[trk_flag][alpha][mu] = grid_rk->TJb[0][alpha][mu];